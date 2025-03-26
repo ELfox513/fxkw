b93af0a662f8dc3d1a6d95bccf5d3d71355e8086
Elo <public.ms513@gmail.com>
2025-03-26 11:23:59 +0300

    Change repository name

--- scripts/513_I2Collection.lua
+++
@@ -1,8 +0,0 @@
---[[ Particly worked: Conflict with LevelStagesEditor save feature
--- Add workspace stage
-local stage = C.BasicLevelStage.new("i2cw", C.Color.new_4f(0,0.625,0,1), "I2Collection Workspace")
-C.Game.i.basicLevelManager:addStage(stage)
--- Set stage from end to beggining of stage list
-local stagesOrdered = C.Game.i.basicLevelManager.stagesOrdered
-stagesOrdered:pop()
-stagesOrdered:insert(0, stage)]]
